c2593b71a76ce03550a964a07af4856973d84d0c
Robert Johnstone <rob@plasticviking.com>
2025-02-03 13:00:24 -0400

    Adjust header and footer, table scroll

--- frontend/_extensions/bcds/bcds.lua
+++ frontend/_extensions/bcds/bcds.lua
@@ -1,40 +1,42 @@
+--local pandoc = require(pandoc)
+--local quarto = require(quarto)
+
 -- Main Accordion Function Shortcode
 return {
   ["callout_start"] = function(args, kwargs, meta)
-      local title = pandoc.utils.stringify(kwargs["title"])
-      local variant = pandoc.utils.stringify(kwargs["variant"])
+    local title = pandoc.utils.stringify(kwargs["title"])
+    local variant = pandoc.utils.stringify(kwargs["variant"])
 
-      if (variant == "") then
-        variant = "lightGrey"
-       end
+    if (variant == "") then
+      variant = "lightGrey"
+    end
 
-      -- todo: aria-labelledby (generate IDs)
+    -- todo: aria-labelledby (generate IDs)
 
-      if quarto.doc.is_format("html:js") then
-        local start_code = "<div class=\"bcds-Callout ".. variant .."\" role=\"note\">"
-        start_code = start_code .. "<div class=\"bcds-Callout--Container\">"
+    if quarto.doc.is_format("html:js") then
+      local markup = "<div class=\"bcds-Callout " .. variant .. "\" role=\"note\">"
+      markup = markup .. "<div class=\"bcds-Callout--Container\">"
 
-        if (title ~= nil) then
-          start_code = start_code .. "<div class=\"bcds-Callout--Title\">"
-          start_code = start_code .. title
-          start_code = start_code .. "</div>"
-         end
-
-        start_code = start_code .. "<div class=\"bcds-Callout--Description\">"
-
-        return pandoc.RawInline("html", start_code)
-      else
-        return pandoc.Null()
+      if (title ~= nil) then
+        markup = markup .. "<div class=\"bcds-Callout--Title\">"
+        markup = markup .. title
+        markup = markup .. "</div>"
       end
+
+      markup = markup .. "<div class=\"bcds-Callout--Description\">"
+
+      return pandoc.RawInline("html", markup)
+    else
+      return pandoc.Null()
+    end
   end,
 
   ["callout_end"] = function(args, kwargs, meta)
-      if quarto.doc.is_format("html:js") then
-        local end_code = "</div></div></div>"
-        return pandoc.RawInline("html", end_code)
-      else
-        return pandoc.Null()
-      end
+    if quarto.doc.is_format("html:js") then
+      local markup = "</div></div></div>"
+      return pandoc.RawInline("html", markup)
+    else
+      return pandoc.Null()
+    end
   end
-
 }
